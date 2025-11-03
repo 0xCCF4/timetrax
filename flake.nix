a8bce2315693dedbd978448ee8e21009dc9f8df1
0xCCF4 <0xccf4-git-commit@lenzentech.de>
2025-11-03 07:55:45 +0100

    wip: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "PhotoSort";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ flake-parts, naersk, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, lib, system, ... }:
+        let
+          naersk' = pkgs.callPackage naersk { };
+        in
+        rec {
+          packages.default = naersk'.buildPackage {
+            src = ./.;
+            gitAllRefs = true;
+          };
+          packages.tt = pkgs.writeShellScriptBin "tt" ''exec ${lib.getExe packages.default} "$@"'';
+        };
+    };
+}
